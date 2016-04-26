950924bda1dd0109a31cce8643266fe790714ee8
DanilTol <d.a.tolmachov@gmail.com>
2016-04-26 19:16:52 +0300

    half

---
+++ GradeParser.BL/ExelParser/StudentParse.cs
@@ -0,0 +1,13 @@
+﻿using GradeParser.BL.Data.Model;
+
+namespace GradeParser.BL.ExelParser
+{
+    public class StudentParse
+    {
+        private string ExamTypeString = "екзамен";
+        private string OffsetTypeString = "залік";
+        private string DiffOffsetString = "диф. залік";
+
+        public Student Parse
+    }
+}
